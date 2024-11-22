d2f5cb7a44c252903b253e9af57731a599f65122
NikhilRaj2004 <134911633+NikhilRaj2004@users.noreply.github.com>
2024-11-22 13:08:31 +0530

    Add files via upload

--- resistreact.py
+++ resistreact.py
@@ -2,19 +2,20 @@ import streamlit as st
 import math
 
 def Tran_Eff(VSC, ISC, WSC):
-    ZSC = VSC / ISC
-    R1 = WSC / (ISC ** 2)
+    ZSC = (VSC) / ISC
+    R1 = (WSC) / (ISC ** 2)
     X1 = math.sqrt(ZSC ** 2 - R1 ** 2)
     return R1, X1
 
-st.title("02341A0259-PS8")
+st.title("02341A0259-PS8(2205A21032 SARIMALLA NIKHIL RAJ)")
 st.header("Transformer Winding Resistance and Reactance Calculator")
+st.subheader("Calculate the winding resistance (R1),and reactance (x1) of a transformer based on short circuit test measurements like VSC,ISC and WSC:")
 
 VSC = st.number_input("Enter VSC (in Volts):", min_value=0.0, step=1.0)
 ISC = st.number_input("Enter ISC (in Amps):", min_value=0.0, step=1.0)
-WSC = st.number_input("Enter WSC (in Watts):", min_value=0.0, step=1.0)
+WSC = st.number_input("Enter WSC (in  Watts):", min_value=0.0, step=1.0)
 
-if st.button("Compute"):
+if st.button("Compute & Run"):
     if ISC > 0:
         R1, X1 = Tran_Eff(VSC, ISC, WSC)
         st.write(f"Winding Resistance (R1): {R1:.4f} Ohms")
